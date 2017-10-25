f36909aee34cb838c534721ac074b06846364f7c
Hack Reactor Student <student@hackreactor.com>
2017-10-25 15:43:04 -0700

    Completed VideoList and VideoListEntry rendering

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,8 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map((video, index) => 
+      <VideoListEntry video = {video} key = {index}/>
+    )}
   </div>
 );
 
