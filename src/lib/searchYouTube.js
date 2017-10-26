636d3f1678a3b8f7410ee34b49cce5a83c7ba871
Hack Reactor Student <student@hackreactor.com>
2017-10-25 18:51:31 -0700

    search youtube tests passing

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,30 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  console.log(options);
+  var data = {
+    q: options.query,
+    key: options.key,
+    maxResults: options.max,
+    part: 'snippet',
+    type: 'video',
+    videoEmbeddable: 'true'
+  };
+  
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: data,
+    
+    success: function(data) {
+      console.log('success');
+      callback(data.items);
+
+ 
+    },
+    
+    error: function(data) {
+      console.log('failure', data);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
